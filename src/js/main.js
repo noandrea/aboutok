481ee3f9e04cceef30c6763a44d2917ac50b6e49
Enrico Icardi <enrico.icardi@gmail.com>
2018-08-13 22:52:41 +0200

    New Copy. It all sounded cockier before

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import '../css/main.css'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime'
 OfflinePluginRuntime.install()
+
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const d = new Date()
+document.querySelector('.month').innerHTML = month[d.getMonth()]
+
+const storyChars = document.querySelector('.story').innerHTML.replace(/<[^>]+>/ig, '')
+const charCountField = document.querySelector('.tot-count')
+const charCount = storyChars.split(' ').filter((n) => { return n !== '' }).length - 1 // ignore the word "Me"
+charCountField.innerHTML = charCount
