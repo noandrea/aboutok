095d38a9b6a8c825b916c6861ca96968a7942a88
Enrico Icardi <enrico.icardi@gmail.com>
2019-01-14 21:59:01 +0100

    2019 Update

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ OfflinePluginRuntime.install()
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const d = new Date()
 document.querySelector('.month').innerHTML = month[d.getMonth()]
+document.querySelector('.year').innerHTML = d.getFullYear()
+
+document.querySelector('.years').innerHTML = (d.getFullYear() - 2006)
 
 const storyChars = document.querySelector('.story').innerHTML.replace(/<[^>]+>/ig, '')
 const charCountField = document.querySelector('.tot-count')
